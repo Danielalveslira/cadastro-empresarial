85ef9ba6af4ddf00cea3525cea41870b9af31147
Alan C <alancsystem@gmail.com>
2019-08-28 15:07:19 -0300

    Modificações no rodapé
    - Cor do coração modificado para azul
    - Adicionado "e Alan" com link para meu site

--- frontend/src/components/template/Footer.jsx
+++ frontend/src/components/template/Footer.jsx
@@ -4,7 +4,13 @@ import './Footer.css'
 export default props =>
   <footer className="Footer">
     <span>
-      Desenvolvido com <i className="fa fa-heart text-danger"></i> por <strong>Tecnologia Senai</strong>
+      Desenvolvido com <i className="fa fa-heart text-info"></i> por <strong>Tecnologia Senai </strong>
+      e <strong className="text-primary">
+          <a href="https://nerd0000.github.io/" target="_blank" rel="noopener noreferrer">
+            Alan
+          </a>
+        </strong>
     </span>
   </footer>
-//"fa fa-heart text-danger"
+//"fa fa-heart text-danger / text-primary se refere ao bootstrap"
+//rel="noopener noreferrer" evita erros de segurança

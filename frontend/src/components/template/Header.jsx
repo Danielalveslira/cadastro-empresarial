18b266f9142c46523c0ee5ada646af35cc57bfd1
Alan C <alancsystem@gmail.com>
2019-08-30 17:28:52 -0300

    cor vermelha icone

--- frontend/src/components/template/Header.jsx
+++ frontend/src/components/template/Header.jsx
@@ -4,7 +4,7 @@ import './Header.css'
 export default props =>
    <header className="header d-nome d-sm-flex flex-column">
      <h1 className="mt -3">
-        <i className={`fa fa-${props.icon}`}></i> {props.title}
+        <i className={`fa fa-${props.icon} text-danger`}></i> {props.title}
         <br />
          <p>{props.subtitle}</p>
      </h1>

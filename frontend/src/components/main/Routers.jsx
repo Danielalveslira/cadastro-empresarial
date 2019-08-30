c5005eef970e84bd01a0afbdc65b30d2b2616ade
Alan C <alancsystem@gmail.com>
2019-08-30 16:51:22 -0300

    Criado tela de cadastro de produtos

--- frontend/src/components/main/Routers.jsx
+++ frontend/src/components/main/Routers.jsx
@@ -3,11 +3,13 @@ import { Switch, Route, Redirect } from 'react-router'
 //escolha, rota. redirecionar
 import Home from '../home/Home'
 import UserCrud from '../user/UserCrud'
+import ProductCrud from '../products/ProductCrud'
 
 export default props =>
     <Switch>
         <Route exact path='/' component={Home}/>
         <Route path='/User' component={UserCrud} />
+        <Route path='/Product' component={ProductCrud} />
         <Redirect from= '*' to= '/'/>
     </Switch>
 /*Reference:ferramentas do React Router.

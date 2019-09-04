147a392152a6f23905168ce745f193239425250d
Alan C <alancsystem@gmail.com>
2019-09-04 17:14:26 -0300

    Navegação do menu adiciona fornecedores e vendas

--- frontend/src/components/template/Nav.jsx
+++ frontend/src/components/template/Nav.jsx
@@ -2,16 +2,22 @@ import React from 'react'
 import './Nav.css'
 
 export default props =>
-  <aside className = "menu-area">
-      <nav className = "menu">
-		<a href = '#/'>
-			<i className = "fa fa-home"></i>Início
+	<aside className="menu-area">
+		<nav className="menu">
+			<a href='#/'>
+				<i className="fa fa-home"></i> Início
 		</a>
-		<a href = '#/user'>
-			<i className = "fa fa-user"></i>Usuários
+			<a href='#/user'>
+				<i className="fa fa-user"></i> Usuários
 		</a>
 			<a href='#/Product'>
-				<i className="fa fa-shopping-cart"></i>Produtos
+				<i className="fa fa-shopping-cart"></i> Produtos
 		</a>
-      </nav>
+			<a href='#/sales'>
+				<i className="fa fa-money"></i> Vendas
+		</a>
+			<a href='#/provider'>
+				<i className="fa fa-archive"></i> Fornecedores
+		</a>
+		</nav>
 	</aside>

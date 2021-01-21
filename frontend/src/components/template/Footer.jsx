cb51fdc6d013745c9c90349244eb552c7d0e00c7
Alan C <alancsystem@gmail.com>
2021-01-21 17:03:08 +0000

    update..

--- frontend/src/components/template/Footer.jsx
+++ frontend/src/components/template/Footer.jsx
@@ -4,13 +4,9 @@ import './Footer.css'
 export default props =>
   <footer className="Footer">
     <span>
-      Desenvolvido com <i className="fa fa-heart text-info"></i> por <strong>Tecnologia Senai </strong>
-      e <strong className="text-primary">
-          <a href="https://nerd0000.github.io/" target="_blank" rel="noopener noreferrer">
-            Alan
-          </a>
-        </strong>
+      Desenvolvido com <i className="fa fa-heart text-info"></i> por <strong>Tecnologia Senai </strong> e 
+      <strong className="text-primary"> 
+        <a href="https://nerd00f.github.io/" target="_blank" rel="noopener noreferrer"> Alan </a>
+      </strong>
     </span>
   </footer>
-//"fa fa-heart text-danger / text-primary se refere ao bootstrap"
-//rel="noopener noreferrer" evita erros de segurança
